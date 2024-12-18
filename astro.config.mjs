b797b607dc34b24a272e5322219055f7aa79f053
Luca Di Gianni <luca.dgni@gmail.com>
2024-12-18 12:21:08 +0100

    refactor: update site name and configuration from blog to writings

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://blog.ldgni.io/",
+  site: "https://writings.ldgni.io/",
   integrations: [mdx(), sitemap(), tailwind()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -4,11 +4,6 @@ export const SITE: Site = {
   NAME: "My writings",
 };
 
-export const HOME: Metadata = {
-  TITLE: "Luca Di Gianni",
-  DESCRIPTION: "I'm an IT technician and web developer",
-};
-
 export const BLOG: Metadata = {
   TITLE: "My writings",
   DESCRIPTION: "A collection of articles on topics I am passionate about",
