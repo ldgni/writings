a93880fc6703c60b301f5670cc0870acd32bc7cc
Luca Di Gianni <luca.dgni@gmail.com>
2024-12-19 14:47:29 +0100

    style: update font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        sans: ["Inter Variable", ...defaultTheme.fontFamily.sans],
       },
     },
   },
