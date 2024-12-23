82c9851726b4066d4c9e77e3c36840426db78432
Luca Di Gianni <luca.dgni@gmail.com>
2024-12-23 22:39:18 +0100

    fix: update blog description to include 'software' for better relevance

--- src/consts.ts
+++ src/consts.ts
@@ -3,5 +3,5 @@ import type { Metadata } from "@types";
 export const BLOG: Metadata = {
   TITLE: "Luca Di Gianni",
   DESCRIPTION:
-    "Luca Di Gianni's collection of personal writings on web development, technology, and more.",
+    "Luca Di Gianni's collection of personal writings on web development, software, technology, and more.",
 };
