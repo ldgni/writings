cf54ee0e94d409bce2d9dcbd69c2ac49bd4dfcbd
Luca Di Gianni <luca.dgni@gmail.com>
2024-12-23 22:54:42 +0100

    fix: update blog title for better personalization

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import type { Metadata } from "@types";
 
 export const BLOG: Metadata = {
-  TITLE: "Luca Di Gianni",
+  TITLE: "My personal writings",
   DESCRIPTION:
     "Luca Di Gianni's collection of personal writings on web development, software, technology, and more.",
 };
